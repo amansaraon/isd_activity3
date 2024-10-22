54c26cd1b60665115ab034b1acd5a9880dcae81f
amandeep <amandeepsaraon086@gmail.com>
2024-10-22 14:36:15 -0500

    Define Payment class with strategy validation and bill payment functionality

--- payment/payment.py
+++ payment/payment.py
@@ -1,2 +1,28 @@
-from billing_account.billing_account import BillingAccount
+
+# # from billing_account.billing_account import BillingAccount
+# # from payee.payee import Payee
+# from patterns.strategy.payment_strategy import PaymentStrategy
+# from billing_account.billing_account import BillingAccount
+# from payee.payee import Payee
+
+# class Payment:
+#     def __init__(self, strategy: PaymentStrategy):
+#         if not isinstance(strategy, PaymentStrategy):
+#             raise ValueError("Invalid Strategy")
+#         self.strategy = strategy
+
+#     def pay_bill(self, account: BillingAccount, payee: Payee, amount: float) -> str:
+#         return self.strategy.process_payment(account, payee, amount)
+from patterns.strategy.payment_strategy import PaymentStrategy
 from payee.payee import Payee
+from billing_account.billing_account import BillingAccount  
+
+class Payment:
+    def __init__(self, strategy: PaymentStrategy):
+        if isinstance(strategy, PaymentStrategy):
+            self.strategy = strategy
+        else:
+            raise ValueError("Invalid Strategy")
+
+    def pay_bill(self, account: BillingAccount, payee: Payee, amount: float)->str:
+        return self.strategy.process_payment(account, payee, amount)
